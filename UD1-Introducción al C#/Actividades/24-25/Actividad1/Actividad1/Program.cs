d114b451b7dde5d940e31a556ecd92928284d805
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2024-09-18 12:55:29 +0200

    UD1

---
+++ UD1-Introducción al C#/Actividades/24-25/Actividad1/Actividad1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Actividad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!");
+            Console.ReadKey();
+        }
+    }
+}
