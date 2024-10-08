86602b126cfe86df9aef2f81a2ae38a8f0945ab6
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2024-10-08 17:59:40 +0200

    UD2
    UD2

---
+++ UD2-C# avanzado/Actividades/24-25/Actividad1/Actividad1/Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad1
+{
+    public class Persona
+    {
+        //Atributos de la clase
+        public string nombre;
+        public string apellidos;
+        public int edad;
+        public string genero;
+        public string direccion;
+        public string email;
+
+        //Metodos de la clase
+
+        public string Presentarse()
+        {
+            return $"hola, soy {nombre} {apellidos}";
+        }
+
+        public void EnviarCorreo(string mensaje)
+        {
+            Console.WriteLine("El correo dice que:");
+            Console.WriteLine(mensaje);
+        }
+
+        public bool EsMayorDeEdad()
+        {
+            if (edad >= 18)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ UD2-C# avanzado/Actividades/24-25/Actividad1/Actividad1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Actividad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona profe1 = new Persona();
+            Persona profe2 = new Persona();
+            Persona profe3 = new Persona();
+
+            profe1.nombre = "Tacio";
+            profe1.apellidos = "Camba";
+            profe1.edad = 30;
+
+            Console.WriteLine(profe1.Presentarse());
+            profe1.EnviarCorreo("Hola mundo");
+            Console.WriteLine("El profe es mayor de edad:" + profe1.EsMayorDeEdad());
+
+            profe2.nombre = "Ramon";
+            profe2.apellidos = "Rios";
+            profe2.edad = 40;
+
+            Console.WriteLine(profe2.Presentarse());
+            profe2.EnviarCorreo("Hola mundo");
+            Console.WriteLine("El profe es mayor de edad:" + profe2.EsMayorDeEdad());
+
+            profe3.nombre = "Martin";
+            profe3.apellidos = "Garcia";
+            profe3.edad = 30;
+
+            Console.WriteLine(profe3.Presentarse());
+            profe3.EnviarCorreo("Hola mundo");
+            Console.WriteLine("El profe es mayor de edad:" + profe3.EsMayorDeEdad());
+
+            Console.ReadLine();
+        }
+    }
+}
