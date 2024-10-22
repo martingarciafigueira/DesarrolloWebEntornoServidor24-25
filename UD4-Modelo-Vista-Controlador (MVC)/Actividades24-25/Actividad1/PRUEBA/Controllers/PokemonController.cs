d4babffdf42d63d5625310693bf3c8769f5a89df
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2024-10-22 18:05:39 +0200

    UD3

---
+++ UD4-Modelo-Vista-Controlador (MVC)/Actividades24-25/Actividad1/PRUEBA/Controllers/PokemonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRUEBA.Models;
+
+namespace PRUEBA.Controllers
+{
+    public class PokemonController : Controller
+    {
+        //Añado las dos acciones
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        public IActionResult VerPokemon()
+        {
+            Pokemon p1 = new Pokemon();
+            p1.PokemonId = 1;
+            p1.Nombre = "Pikachu";
+            p1.Altura = 0.5f;
+            p1.Peso = 5;
+
+            return View(p1);
+        }
+    }
+}
---
+++ UD4-Modelo-Vista-Controlador (MVC)/Actividades24-25/Actividad1/PRUEBA/Models/Pokemon.cs
@@ -0,0 +1,10 @@
+﻿namespace PRUEBA.Models
+{
+    public class Pokemon
+    {
+        public int PokemonId { get; set; }
+        public string Nombre { get; set; }
+        public float Peso { get; set; }
+        public float Altura { get; set; }
+    }
+}
