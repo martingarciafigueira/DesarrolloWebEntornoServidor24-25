7ebe8c59c093e508d921059de8b162a84095a58c
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2024-11-26 17:26:03 +0100

    ActividadesTema6

---
+++ UD6-El Controlador/Actividades/24-25/Actividad1/Actividad1/Controllers/ProductoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad1.Controllers
+{
+	public class ProductoController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Listar()
+		{
+			return Content("Muestro una lista de productos");
+		}
+		public IActionResult Detalles(int id)
+		{
+			return Content("Muestro los detalles del producto con ID: " + id);
+		}
+		public IActionResult Buscar(string nombre)
+		{
+			return Content("Busco productos que tengan este nombre: " + nombre);
+		}
+	}
+}
